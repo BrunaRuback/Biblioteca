f7a19fe380677f6a503aadd10af10c7beed11cbd
bruna <brunaruback07@gmail.com>
2023-01-04 20:14:21 -0300

    :sparkles: feat: criando classe 'Livro'

--- Livro.java
+++ Livro.java
@@ -1,4 +1,48 @@
+import java.util.Date;
+
 public class Livro 
 {
+    private String titulo;
+    private String editora;
+    private String autor;
+    private Date dataPublicaçao;
     
+    public Livro(String titulo, String editora, String autor, Date dataPublicaçao) {
+        this.titulo = titulo;
+        this.editora = editora;
+        this.autor = autor;
+        this.dataPublicaçao = dataPublicaçao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Date getDataPublicaçao() {
+        return dataPublicaçao;
+    }
+
+    public void setDataPublicaçao(Date dataPublicaçao) {
+        this.dataPublicaçao = dataPublicaçao;
+    }
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+COMANDOS GIT:
+
+- git checkout -b [nome-da-branch]: cria uma nova branch;
+- git branch: lista todas as branchs;
+- git checkout [nome-da-branch]: move para uma branch existente;
+- git add [caminho]: adiciona os arquivos na árvore de alterações;
+    - Ex: git add .
+- git commit -m "mensagem": commita as alteraçoes;
+
+Como fazer commit: https://www.conventionalcommits.org/en/v1.0.0/
