b728fe6280b4d9aa401c5f9831a9303f48359857
bruna <brunaruback07@gmail.com>
2023-01-04 20:19:33 -0300

    :sparkles: feat: criando classe 'Biblioteca' e implementando método 'cadastrarLivro'

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
 public class Biblioteca 
 {
-    
+    private ArrayList<Livro> livros;
+
+    public Biblioteca()
+    {
+        this.livros = new ArrayList<>();
+    }
+
+    public void cadastrarLivro(Livro livro)
+    {
+        this.livros.add(livro);
+    }
 }
